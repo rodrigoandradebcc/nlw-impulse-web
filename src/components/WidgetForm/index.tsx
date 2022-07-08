219cc31f3f2723678923cb3d700bb04db8eb7b56
Rod Andrade <rodrigoandradebcc@gmail.com>
2022-07-08 17:04:12 -0300

    feature: second class - part I

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,15 @@
+export function WidgetForm() {
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+            </header>
+
+            <p>Hello Word</p>
+
+            <footer>
+
+            </footer>
+        </div>
+    );
+}0
